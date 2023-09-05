88b60892d19d040735b08d3f3c64019fa9b946a1
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-09-05 22:01:19 +0400

    Adding CVC input

--- src/components/cvc-input/CvcInput.tsx
+++ src/components/cvc-input/CvcInput.tsx
@@ -8,8 +8,23 @@ const CvcInput: FC<CvcInputProps> = () =>
 {
     return (
         <>
-        <div>
-           Cvc input 
+        <div className={`
+         flex
+         flex-col
+         gap-y-2.5
+        `}>
+           <h2 className={`
+            input-title
+           `}>cardholder name</h2>
+           <input 
+           type="text"
+           placeholder='cvc' 
+           className={`
+            input-style
+            w-48
+            py-3
+            pl-4
+           `} /> 
         </div>
         </>
     )
