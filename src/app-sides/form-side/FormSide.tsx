6c9c25249b1a6f99a7545c5118bee2e84fad2321
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-09-11 11:49:20 +0400

    Adding confirmed form style and structure

--- src/app-sides/form-side/FormSide.tsx
+++ src/app-sides/form-side/FormSide.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { NameInput, NumberInput, CvcInput, ConfirmBtn, DateInput } from '../../components/main';
 import { useMyContext } from '../../App';
-
+import completeIcon from '../../assets/icon-complete.svg';
 interface FormSideProps {
 
 }
@@ -13,12 +13,47 @@ const FormSide: FC<FormSideProps> = ({}) =>
         <>
         {
         
-        formSubmited 
+        formSubmited
         
         ? 
             
-            <div>
-                formSubmited
+            <div className='
+             flex
+             flex-col
+             items-center
+            '>
+                <img src={completeIcon} alt='Complete sign'/>
+                <h2 className='
+                 text-3xl
+                 font-normal
+                 font-medium
+                 leading-normal
+                 tracking-wide
+                 uppercase
+                 text-deepViolet
+                 mt-[35px]
+                 mb-[16px]
+                '>thank you!</h2>
+                <span className='
+                 text-lg
+                 font-medium
+                 leading-normal
+                 text-commonGrey
+                 mb-[45px]
+                '>We’ve added your card details</span>
+                <button 
+                type='button' 
+                className={`
+                    rounded-[8px]
+                    py-4
+                    px-40
+                    text-commonWhite
+                    bg-deepViolet
+                    capitalize
+                    cursor-pointer
+                    `}>
+                    continue
+                </button>
            </div>
         
         : 
